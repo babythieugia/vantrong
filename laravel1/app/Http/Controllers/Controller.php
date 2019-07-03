029c50f27a87d287bbf3e08398c85d4fe410ba29
vantrong2181998@gmail.com <vantrong98>
2019-07-03 14:28:22 +0700

    test

---
+++ laravel1/app/Http/Controllers/Controller.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+}
+
+/**
+ * 5->8 : Nạp namespace
+ * 12 : nap case vào
+ */
